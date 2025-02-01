56a70aa538bddd424d6028be7c59923b479c22a0
Karen17Mendoza <melanie171095@gmail.com>
2025-01-31 23:01:52 -0500

    First changes

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Hora del Desafío";
 
+function metodoUno() {
+    console.log("El botón fue clicado");
+}
+function metodoDos() {
+    let ciudad = prompt("Escribe el nombre de alguna ciudad de Brasil");
+    alert(`Estuve en   ${ciudad}  y me acorde de ti`);
+}
+function metodoTres() {
+    alert("Yo amo JS");
+}
+function metodoSuma() {
+    let valor1 = parseInt(prompt("Ingresa el primer numero"));
+    let valor2 = parseInt(prompt("Ingresa el segundo numero"));
+    let suma = valor1 + valor2;
+    alert(`La suma es: ${suma}`);
+}
